70b46834614b92e41af6496dc3a040fa058ef866
lvpeng01 <lvpeng01@baidu.com>
2020-08-06 20:14:44 +0800

    init 1

---
+++ gitversion/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @author : lvpeng01
+ * @since : 2020/8/6
+ **/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+}
